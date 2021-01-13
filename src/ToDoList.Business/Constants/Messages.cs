b5ac41076aebbbff1cfd4f91ada932a62235d52d
ugurdemir <ugurdemir551@gmail.com>
2021-01-13 03:00:37 +0300

    Error&Success Messages Created

---
+++ src/ToDoList.Business/Constants/Messages.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProjectAdded = "Proje Başarıyla Eklendi !";
+        public static string ProjectDeleted = "Proje Başarıyla Silindi !";
+        public static string ProjectUpdated = "Proje Başarıyla Güncellendi !";
+        public static string ProjectList = "Proje Başarıyla Listendi !";
+        public static string ProjectGet = "Proje Başarıyla Getirildi !";
+
+        public static string ProjectAddingError = "Proje Eklenemedi !";
+        public static string ProjectDeletingError = "Proje Silinemedi !";
+
+
+
+    }
+}
