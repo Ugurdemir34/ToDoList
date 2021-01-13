562fa2df22159c6e95bb59296ff5729c893a8252
ugurdemir <ugurdemir551@gmail.com>
2021-01-13 03:00:20 +0300

    Project Manager Added

---
+++ src/ToDoList.Business/Abstract/IProjectService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ToDoList.Core.Utilities.Results;
+using ToDoList.Entities.Concrete;
+
+namespace ToDoList.Business.Abstract
+{
+    public interface IProjectService
+    {
+        IDataResult<List<Project>> GetAll();
+        IResult Add(Project project);
+        IResult Update(Project project);
+        IResult Delete(Project project);
+        IDataResult<Project> GetProject(Guid projectId);
+
+    }
+}
---
+++ src/ToDoList.Business/Concrete/ProjectManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Business.Abstract;
+using ToDoList.Business.Constants;
+using ToDoList.Core.Utilities.Results;
+using ToDoList.DataAccess.Abstract;
+using ToDoList.Entities.Concrete;
+
+namespace ToDoList.Business.Concrete
+{
+    public class ProjectManager : IProjectService
+    {
+        private IProjectDal _projectDal;
+        public ProjectManager(IProjectDal projectDal)
+        {
+            _projectDal = projectDal;
+        }
+        public IResult Add(Project project)
+        {
+            if(project==null)
+            {
+                return new ErrorResult(Messages.ProjectAddingError);
+            }
+            return new SuccessResult(Messages.ProjectAdded);
+        }
+
+        public IResult Delete(Project project)
+        {
+            if (project == null)
+            {
+                return new ErrorResult(Messages.ProjectDeletingError);
+            }
+            return new SuccessResult(Messages.ProjectDeleted);
+        }
+
+        public IDataResult<List<Project>> GetAll()
+        {
+            var model = _projectDal.GetList();
+            return new SuccessDataResult<List<Project>>(model, Messages.ProjectList);
+        }
+
+        public IDataResult<Project> GetProject(Guid projectId)
+        {
+            var model = _projectDal.Get(i=>i.Id==projectId);
+            return new SuccessDataResult<Project>(model, Messages.ProjectGet);
+        }
+
+        public IResult Update(Project project)
+        {
+            return new SuccessResult(Messages.ProjectUpdated);
+        }
+    }
+}
