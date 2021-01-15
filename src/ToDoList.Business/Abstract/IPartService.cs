d6c4300de5f545463d5ecf0c6ab4e7d26eafeffb
ugurdemir <ugurdemir551@gmail.com>
2021-01-15 19:09:35 +0300

    Created Part Manager

---
+++ src/ToDoList.Business/Abstract/IPartService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Core.Utilities.Results;
+using ToDoList.Entities.Concrete;
+
+namespace ToDoList.Business.Abstract
+{
+    public interface IPartService
+    {
+        IDataResult<List<Part>> GetAll();
+        IResult Add(Part part);
+        IResult Update(Part part);
+        IResult Delete(Part part);
+        IDataResult<Part> GetPart(Guid partId);
+        IDataResult<Part> GetPartByProjectId(Guid projectId);
+    }
+}
---
+++ src/ToDoList.Business/Concrete/PartManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Business.Abstract;
+using ToDoList.Business.Constants;
+using ToDoList.Core.Utilities.Results;
+using ToDoList.DataAccess.Abstract;
+using ToDoList.Entities.Concrete;
+
+namespace ToDoList.Business.Concrete
+{
+    public class PartManager : IPartService
+    {
+        private IPartDal _partDal;
+        public PartManager(IPartDal partDal)
+        {
+            _partDal = partDal;
+        }
+        public IResult Add(Part part)
+        {
+            if (part == null)
+            {
+                return new ErrorResult(Messages.PartAddingError);
+            }
+            return new SuccessResult(Messages.PartAdded);
+        }
+
+        public IResult Delete(Part part)
+        {
+            if (part == null)
+            {
+                return new ErrorResult(Messages.PartDeletingError);
+            }
+            return new SuccessResult(Messages.PartDeleted);
+        }
+
+        public IDataResult<List<Part>> GetAll()
+        {
+            try
+            {
+                var model = _partDal.GetList();
+                
+                return new SuccessDataResult<List<Part>>(model, Messages.PartList);
+            }
+            catch (Exception ex)
+            {
+
+                return new ErrorDataResult<List<Part>>(ex.Message);
+            }
+        }
+
+        public IDataResult<Part> GetPart(Guid partId)
+        {
+            try
+            {
+                var model = _partDal.Get(i => i.Id == partId);             
+                return new SuccessDataResult<Part>(Messages.PartGet);
+            }
+            catch (Exception ex)
+            {
+                /*  if (model == null)
+                 {
+                     return new ErrorDataResult<Part>(Messages.PartGetError);
+                 }*/
+                return new ErrorDataResult<Part>(ex.Message);
+            }
+        }
+        public IDataResult<Part> GetPartByProjectId(Guid projectId)
+        {
+            try
+            {
+                var model = _partDal.Get(i=>i.Project.Id == projectId);
+
+                return new SuccessDataResult<Part>(model, Messages.PartGet);
+            }
+            catch (Exception ex)
+            {
+
+                return new ErrorDataResult<Part>(ex.Message);
+            }
+        }
+        public IResult Update(Part part)
+        {
+            if (part == null)
+            {
+                return new ErrorResult(Messages.PartUpdatingError);
+            }
+            return new SuccessResult(Messages.PartUpdated);
+        }
+    }
+}
