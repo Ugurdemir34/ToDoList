eff022e6eeb64c01ad0b7b27058863659e51b91e
ugurdemir <ugurdemir551@gmail.com>
2021-01-15 19:10:31 +0300

    Created

---
+++ src/ToDoList.DataAccess/Concrete/EntityFramework/EfPartDal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Core.DataAccess.EntityFramework;
+using ToDoList.DataAccess.Abstract;
+using ToDoList.Entities.Concrete;
+
+namespace ToDoList.DataAccess.Concrete.EntityFramework
+{
+    public class EfPartDal : EfEntityRepositoryBase<Part, ToDoListContext>, IPartDal
+    {
+    }
+}
---
+++ src/ToDoList.WebAPI/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Entities.Concrete;
+using ToDoList.Entities.Dtos;
+
+namespace ToDoList.WebAPI.MappingProfiles
+{
+    public class MappingProfile :Profile
+    {
+        public MappingProfile()
+        {
+           
+        }
+    }
+}
