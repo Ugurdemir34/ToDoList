7b36dd01b7152003a13915faadce8de6a6679bb7
ugurdemir <ugurdemir551@gmail.com>
2021-01-16 22:22:10 +0300

    Created

---
+++ src/ToDoList.WebAPI/Controllers/PartController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDoList.Business.Abstract;
+
+namespace ToDoList.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PartController : ControllerBase
+    {
+        private IPartService _partService;
+        private readonly ILogger<WeatherForecastController> _logger;
+        public PartController(ILogger<WeatherForecastController> logger, IPartService partService)
+        {
+            _logger = logger;
+            _partService = partService;
+        }
+        [HttpGet]
+        public IActionResult GetAllParts()
+        {
+            var model = _partService.GetAll();
+            return Ok(model);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetPartByProjectId(Guid id)
+        {
+            var model = _partService.GetPartByProjectId(id);
+            return Ok(model);
+        }
+    }
+}
---
+++ src/ToDoList.WebAPI/Controllers/ProjectController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDoList.Business.Abstract;
+
+namespace ToDoList.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private IProjectService _projectService;
+        private readonly ILogger<WeatherForecastController> _logger;
+        public ProjectController(ILogger<WeatherForecastController> logger, IProjectService projectService)
+        {
+            _logger = logger;
+            _projectService = projectService;
+        }
+        [HttpGet]
+        public IActionResult GetAllProjects()
+        {
+            var model = _projectService.GetAll();
+            return Ok(model);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProject(Guid id)
+        {
+            var model = _projectService.GetProject(id);
+            return Ok(model);
+        }
+    }
+}
