68b146db38028566a5723b30065187612c44f8e2
ugurdemir <ugurdemir551@gmail.com>
2021-02-22 02:22:25 +0300

    AccessToken Created

---
+++ src/ToDoList.DataAccess/Security/Jwt/AccessToken.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.DataAccess.Security.Jwt
+{
+    public class AccessToken
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
