2878cb661a9f62dd83ea73b09b3d40adb71cb885
ugurdemir <ugurdemir551@gmail.com>
2021-02-22 02:19:59 +0300

    IToken Helper Created

---
+++ src/ToDoList.DataAccess/Security/Jwt/ITokenHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoList.Entities.Concrete;
+
+namespace ToDoList.DataAccess.Security.Jwt
+{
+    public interface ITokenHelper
+    {
+        AccessToken CreateToken(User user, List<OperationClaim> operationClaim);
+    }
+}
